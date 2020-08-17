3b5969da6ae084ca05ab1595391e704d200e2f65
smooney211993 <sm.athome21@gmail.com>
2020-08-17 17:34:37 +1000

    finished and ready to be deployed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [quoteData, setQuoteData] = useState({
+    quote: '',
+    author: ''
+  })
+  const [isLoading, setIsLoading] = useState(false)
+
+  const getQuote = async () =>{
+    const proxy = "https://quoteserver1.herokuapp.com/";
+    const apiUrl = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+      try{
+        setIsLoading(true);
+        const response = await fetch(proxy+apiUrl);
+        const {quoteText, quoteAuthor} = await response.json();
+        console.log(quoteText);
+        setQuoteData({
+          quote: quoteText,
+          author: quoteAuthor,
+        })
+        setIsLoading(false);
+        
+
+
+      }catch(error){
+        getQuote()
+        console.log(error)
+
+      }
+  }
+  useEffect(()=>{
+  
+
+    getQuote()
+
+  },[])
+
+  const tweetQuote = () => {
+    const {quote, author} = quoteData;
+    const twitterUrl = `https://twitter.com/intent/tweet?$text=${quote} - ${author}`;
+    window.open(twitterUrl,'_blank' ) ;
+}
+
+  const renderQuote = ()=> {
+    if(quoteData.quote.length > 50){
+      return <span id="quote" className="long-quote" >{quoteData.quote}</span>
+   } 
+      return <span id="quote" >{quoteData.quote}</span>
+   }
+
+   const renderLoading = ()=>{
+    if(isLoading){
+      return <div class="loader" id="loader"></div>
+     } 
+
+   }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    isLoading ? renderLoading() :
+   <div className="quote-container" id="quote-container">
+      <div className="quote-text">
+        <i className="fas fa-quote-left">
+        </i>
+        {renderQuote()}
+        <i className="fas fa-quote-right"></i>
+        <div className="quote-author">
+            <span id="author">
+                {quoteData.author}
+            </span>
+        </div>
+        <div className="button-container">
+            <button className="twitter-button" id="twitter" title="Tweet this badboy!" onClick={tweetQuote}>
+                  <i className="fab fa-twitter">
+                  </i>
+            </button>
+            <button id="new-quote" onClick={getQuote}> New Quote</button>
+        </div>
+      </div>
+  </div> 
+  )
 }
 
 export default App;
